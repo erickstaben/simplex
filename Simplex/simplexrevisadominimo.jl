1c28bdabe0fd0a7a802aac5b35375e3e0f12d1b4
Erick Staben <erick@DESKTOP-KP0I2SK.kuikdelivery.com>
2018-01-16 16:59:09 -0200

    mudanças solicitadas

--- Simplex/simplexrevisadominimo.jl
+++ Simplex/simplexrevisadominimo.jl
@@ -1,23 +1,23 @@
-function simplex(z,A,b)
+function simplex(c,A,b)
    m, n = (size(A,1), size(A,2))
-   z = -1*[z ; zeros(m,1)]
+   c = [c ; zeros(m,1)]
    A = [A eye(m)]
    X = 0
    status = [false,false]
    Base,NBase = collect(n+1:n+m),collect(1:n)
    X = A[:,Base]\b
-   println(z)
+   println(c)
     while !(status[1] || status[2])
-          CNbase = z[NBase,:] - A[:,NBase]' *((A[:,Base])'\z[Base,:])
+          CNbase = A[:,NBase]' *((A[:,Base])'\c[Base,:]) - c[NBase,:]
           minNB = findmin(CNbase)
           status[1] = minNB[1] > 0
-          if status[1]
+          if status[1] #optimal solution
               continue
           end
           U = A[:,Base]\A[:,minNB[2]]
           delta = X./U
           status[2] = !any(delta .>0)
-          if status[2]
+          if status[2] #Infinte solution
               continue
           end
         menorpos,ind_menor = minimum(delta[delta.>0]),findfirst(delta,minimum(delta[delta.>0]))
@@ -25,7 +25,6 @@ function simplex(z,A,b)
         X[ind_menor] = delta[ind_menor]
         Base[ind_menor] , NBase[minNB[2]] = (NBase[minNB[2]],Base[ind_menor])
     end
-
 return X
 end
-              #return "Solução ótima? :  " *string(status[1])" Solução Infinita? : "*string(status[2])
+            
